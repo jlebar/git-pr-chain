ffa86a221d17b00b3712b5dfcc6a4dcf558ed4a0
Justin Lebar <jlebar@google.com>
2020-03-15 22:49:52 -0700

    Some refactoring

--- git-pr-chain.py
+++ git-pr-chain.py
@@ -9,21 +9,12 @@ import sys
 import re
 import collections
 import json
+from typing import List, Dict, Tuple
 
 from github import Github  # pip install PyGithub
 from xdg import XDG_CONFIG_HOME  # pip install xdg
 
 VERBOSE = False
-
-
-def gh_access_code():
-    # TODO: Make this user-friendly and interactive.
-    with open(os.path.join(XDG_CONFIG_HOME, 'git-pr-chain.json'), "r") as f:
-        return json.load(f)["access_code"]
-
-
-def gh_session():
-    return Github(gh_access_code())
 
 
 def fatal(msg):
@@ -45,6 +36,16 @@ def traced(fn, show_start=False, show_end=True):
         return ret
 
     return inner
+
+
+def gh_access_code():
+    # TODO: Make this user-friendly and interactive.
+    with open(os.path.join(XDG_CONFIG_HOME, "git-pr-chain.json"), "r") as f:
+        return json.load(f)["access_code"]
+
+
+def gh_session():
+    return Github(gh_access_code())
 
 
 @traced
@@ -76,7 +77,29 @@ def git_tracks(branch=None):
     return git("for-each-ref", "--format=%(upstream:short)", branchref)
 
 
-def parse_md(commit: str):
+@traced
+def branch_commits() -> List[Tuple[str, Dict[str, str]]]:
+    """Get the commits in this branch.
+
+    Returns a list of (commit, {metadata_key, metadata_val}).
+    """
+    # TODO: Check for nonlinear history.
+
+    upstream_branch = git_tracks()
+    if not upstream_branch:
+        fatal(
+            "Set an upstream branch with e.g. `git branch "
+            "--set-upstream-to origin/master`"
+        )
+    split_log = (
+        git("log", "--pretty=%H", f"{upstream_branch}..HEAD").strip().split("\n")
+    )
+
+    # Filter out empty commits (if e.g. the git log produces nothing).
+    return [(c, parse_md(c)) for c in split_log if c]
+
+
+def parse_md(commit: str) -> Dict[str, str]:
     msg = git("show", "-s", "--format=%B", commit)
     lines = [
         re.sub(r"^GPC:\s*", "", l).strip()
@@ -115,32 +138,16 @@ def parse_md(commit: str):
 
 
 def cmd_show(args):
-    # Get the commits in this branch.
-    upstream_branch = git_tracks()
-    if not upstream_branch:
-        fatal(
-            "Set an upstream branch with e.g. `git branch --set-upstream-to origin/master`"
-        )
-    commits = git("log", "--pretty=%H", f"{upstream_branch}..HEAD").strip().split("\n")
-    # Filter out empty commits (if e.g. the git log produces nothing).
-    commits = [c for c in commits if c]
+    commits = branch_commits()
     if not commits:
         fatal(
             "No commits in branch.  Is the upstream branch (git branch "
             "--set-upstream-to <origin/master or something>set correctly?"
         )
 
-    # TODO: Check for nonlinear history.
-
-    # Get the commit message for each commit, keeping only the lines that start
-    # with "GPC:".
-    mds = {}
-    for commit in commits:
-        mds[commit] = parse_md(commit)
-
     # TODO: Make this prettier.
-    for commit in commits:
-        print(git("log", "--oneline", "-n1", commit) + " " + str(mds[commit]))
+    for commit, md in commits:
+        print(git("log", "--oneline", "-n1", commit) + " " + str(md))
 
 
 def cmd_upload(args):
@@ -151,35 +158,35 @@ def main():
     parser = argparse.ArgumentParser()
     subparser = parser.add_subparsers()
 
-    sp_show = subparser.add_parser('show', help='List commits in chain')
+    sp_show = subparser.add_parser("show", help="List commits in chain")
     sp_show.set_defaults(func=cmd_show)
 
-    sp_upload = subparser.add_parser('upload', help='Create and update PRs in github')
-    sp_upload.add_argument('-n', '--dry-run', action='store_true')
+    sp_upload = subparser.add_parser("upload", help="Create and update PRs in github")
+    sp_upload.add_argument("-n", "--dry-run", action="store_true")
     sp_upload.set_defaults(func=cmd_upload)
 
     def cmd_help(args):
-        if 'command' not in args or not args.command:
+        if "command" not in args or not args.command:
             parser.print_help()
-        elif args.command == 'show':
+        elif args.command == "show":
             sp_show.print_help()
-        elif args.command == 'upload':
+        elif args.command == "upload":
             sp_upload.print_help()
-        elif args.command == 'help':
+        elif args.command == "help":
             print("Well aren't you trying to be clever.", file=sys.stderr)
         else:
-            print(f'Unrecognized subcommand {args.command}', file=sys.stderr)
+            print(f"Unrecognized subcommand {args.command}", file=sys.stderr)
             parser.print_help()
             sys.exit(1)
 
-    sp_help = subparser.add_parser('help')
-    sp_help.add_argument('command', nargs='?', help='Subcommand to get help on')
+    sp_help = subparser.add_parser("help")
+    sp_help.add_argument("command", nargs="?", help="Subcommand to get help on")
     sp_help.set_defaults(func=cmd_help)
 
     args = parser.parse_args()
-    if 'func' not in args:
-      parser.print_help()
-      sys.exit(1)
+    if "func" not in args:
+        parser.print_help()
+        sys.exit(1)
 
     args.func(args)
 
